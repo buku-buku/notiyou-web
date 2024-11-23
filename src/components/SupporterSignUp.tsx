86f6f39d6987437a658990e98c2f80b1dbd15d67
grighth12 <grighth12@gmail.com>
2024-11-23 16:08:43 +0900

    feat: dummy 서포터 가입 버튼 이벤트

--- src/components/SupporterSignUp.tsx
+++ src/components/SupporterSignUp.tsx
@@ -1,31 +1,27 @@
 import { useSearch } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
 import { challenger } from "../service/service";
 
 export const SupporterSignUp = () => {
-  const [userId, setUserId] = useState<string | null>(null);
   const userToken = useSearch({
     from: "/",
     select: (search) => search.user,
   });
 
-  console.log(userId);
+  const handleSignUpSupporter = async () => {
+    const user = await challenger.getUser(userToken);
 
-  useEffect(() => {
-    console.log(userToken);
-
-    const getUser = async () => {
-      const user = await challenger.getUser();
-      setUserId(user.id);
-    };
-
-    getUser();
-  }, [userToken]);
+    await Promise.resolve({
+      userId: user.id,
+    });
+  };
 
   return (
     <section className="h-full">
       <div className="container mx-auto h-full flex justify-center items-center">
-        <button className="py-3 px-4 border border-gray-400">
+        <button
+          className="py-3 px-4 border border-gray-400"
+          onClick={handleSignUpSupporter}
+        >
           조력자 회원가입
         </button>
       </div>
--- src/service/service.ts
+++ src/service/service.ts
@@ -1,9 +1,15 @@
+interface User {
+  id: string;
+  userToken: string;
+}
+
 export const challenger = {
-  getUser: async () => {
+  getUser: async (userToken: string): Promise<User> => {
     // return client.get({ url: "/challenger/user" });
 
     return Promise.resolve({
       id: "user1",
+      userToken,
     });
   },
 };
