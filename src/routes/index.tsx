cd38fff244e8d4034347418c6a690d8c79ac30ec
grighth12 <grighth12@gmail.com>
2024-11-23 15:38:42 +0900

    feat: zod를 사용하여 user 파라미터가 없는 경우 접근 불가하도록 막음

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { SupporterSignUp } from "../components/SupporterSignUp";
 
-type SupporterSignUpSearch = {
-  user: string;
-};
+import { z } from "zod";
+
+const supporterSignUpSchema = z.object({
+  user: z.string(),
+});
+
+type SupporterSignUpSearch = z.infer<typeof supporterSignUpSchema>;
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
-  validateSearch: (search): SupporterSignUpSearch => {
-    return {
-      user: (search.user as string) || "",
-    };
-  },
+  validateSearch: (search): SupporterSignUpSearch =>
+    supporterSignUpSchema.parse(search),
 });
 
 function RouteComponent() {
